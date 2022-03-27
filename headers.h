6ed7bebdc44b5e7bfbe58ef2fd3e889fc60b3e3b
matmak13 <maksimowiczmateusz13@gmail.com>
2022-03-27 15:23:57 +0200

    utils.h

--- headers.h
+++ headers.h
@@ -20,8 +20,6 @@ enum
 	RECURSIVE
 } mode = DEFAULT;
 
-long compare_files_times(const char* file1, const char* file2);
-
 void copy_file_dates(const char* from, const char* to);
 void copy_file(const char* from, const char* to, ssize_t buffor, ssize_t large_file_size_limit);
 void delete_file(const char* path);

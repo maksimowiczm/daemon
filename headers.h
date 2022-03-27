5379c2c395164e38edd306759541b706fc51529e
matmak13 <maksimowiczmateusz13@gmail.com>
2022-03-27 13:39:07 +0200

    header file

--- headers.h
+++ headers.h
@@ -1,17 +1,18 @@
 #pragma once
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <stdint.h>
-#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <dirent.h>
-#include <errno.h>
+#include <stdint.h>
 #include <string.h>
-#include <fcntl.h>
-#include <unistd.h>
+#include <errno.h>
+#include <time.h>
 #include <utime.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
 
 enum
 {
@@ -29,8 +30,8 @@ int skip_location(const char* name);
 long compare_files_times(const char* file1, const char* file2);
 
 void copy_file_dates(const char* from, const char* to);
-void copy_file(const char* from, const char* to, ssize_t buffor);
+void copy_file(const char* from, const char* to, ssize_t buffor, ssize_t large_file_size_limit);
 void delete_file(const char* path);
 void delete_directory(const char* path);
 
-void copy_and_delete_all_files(const char* source_path, const char* destination_path, int buffor_size);
+void copy_and_delete_all_files(const char* source_path, const char* destination_path, ssize_t buffor_size, ssize_t large_file_size_limit);

ddf4243f71318d56ccfbacc92bc49ed21499af45
matmak13 <maksimowiczmateusz13@gmail.com>
2022-03-27 15:34:53 +0200

    files.h

---
+++ files.c
@@ -0,0 +1,115 @@
+#include "files.h"
+#include "utils.h"
+
+void copy_file_dates(const char* from, const char* to)
+{
+	struct stat st;
+	stat(from, &st);
+	const long mtime = st.st_mtime;
+	struct utimbuf ubuf;
+	time(&ubuf.actime);
+	ubuf.modtime = mtime;
+	const int err = utime(to, &ubuf);
+	if (err < 0)
+	{
+		printf("copy_file_dates() utime() %s %s %s", from, to, strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+}
+
+void copy_file(const char* from, const char* to, const ssize_t buffor, const ssize_t large_file_size_limit)
+{
+	const int src = open(from, O_RDONLY);
+	if (src < 0)
+	{
+		printf("copy_file() open(from) %s %s", from, strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+	const int dst = open(to, O_WRONLY | O_CREAT | O_APPEND, 0666);
+	if (dst < 0)
+	{
+		printf("copy_file() open(to) %s %s", to, strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+
+	struct stat st;
+	stat(from, &st);
+	const ssize_t size = st.st_size;
+
+	if (size > large_file_size_limit)
+	{
+		char* addr = mmap(NULL, size, PROT_READ, MAP_PRIVATE, src, 0);
+		if (addr == MAP_FAILED)
+		{
+			printf("copy_file() mmap() %s", from);
+			exit(EXIT_FAILURE);
+		}
+
+		write(dst, addr, size);
+		munmap(addr, size);
+	}
+	else
+	{
+		void* buf = malloc(buffor);
+		ssize_t bytes_read = read(src, buf, buffor);
+
+		do
+		{
+			write(dst, buf, bytes_read);
+		}
+		while ((bytes_read = read(src, buf, buffor)) != 0);
+
+		free(buf);
+	}
+
+	int err = close(src);
+	if (err < 0)
+	{
+		printf("copy_file() close(src) %s", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+	err = close(dst);
+	if (err < 0)
+	{
+		printf("copy_file() close(dst) %s", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+
+	// Zmiana daty modyfikacji pliku
+	copy_file_dates(from, to);
+}
+
+void delete_file(const char* path)
+{
+	const int err = remove(path);
+
+	if (err < 0)
+	{
+		printf("delete_file() %s %s", path, strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+}
+
+void delete_directory(const char* path)
+{
+	struct dirent** files_list;
+	const int no_of_files = scandir(path, &files_list, NULL, alphasort);
+	char* src;
+
+	for (int i = 0; i < no_of_files; i++)
+	{
+		const char* file_name = files_list[i]->d_name;
+		src = concat_path(path, file_name);
+
+		if (skip_location(file_name))
+			continue;
+
+		if (is_directory(files_list[i]))
+			delete_directory(src);
+
+		delete_file(src);
+		free(src);
+	}
+
+	free(files_list);
+}
--- headers.h
+++ headers.h
@@ -1,28 +1,8 @@
 #pragma once
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <string.h>
-#include <errno.h>
-#include <time.h>
-#include <utime.h>
-#include <fcntl.h>
 #include <dirent.h>
-#include <unistd.h>
-#include <sys/types.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <getopt.h>
+#include <string.h>
 #include <sys/stat.h>
-#include <sys/mman.h>
-
-enum
-{
-	DEFAULT,
-	RECURSIVE
-} mode = DEFAULT;
-
-void copy_file_dates(const char* from, const char* to);
-void copy_file(const char* from, const char* to, ssize_t buffor, ssize_t large_file_size_limit);
-void delete_file(const char* path);
-void delete_directory(const char* path);
-
-void copy_and_delete_all_files(const char* source_path, const char* destination_path, ssize_t buffor_size, ssize_t large_file_size_limit);
+#include <errno.h>

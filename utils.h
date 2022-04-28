bd1903a6e6d2997ecc926d84feb1551b5b11bd12
matmak13 <maksimowiczmateusz13@gmail.com>
2022-04-28 18:46:08 +0200

    docs

--- utils.h
+++ utils.h
@@ -1,8 +1,28 @@
-#pragma once
+﻿#pragma once
 #include <dirent.h>
 
+/**
+ * \brief Sprawdza czy plik jest folderem
+ * \param dir dirent do pliku
+ * \return true - jeśli plik jest folderem\n
+ * false - w przeciwnym wypadku
+ */
 int is_directory(const struct dirent* dir);
+
+/**
+ * \brief Sprawdza czy plik jest zwyklym plikiem
+ * \param path ścieżka do pliku
+ * \param error jeśli true raportowanie błedów uruchomione
+ * \return true - jeśli plik jest zwykłym plikiem \n
+ * false - w przeciwnym wypadku
+ */
 int is_regular_file(const char* path, int error);
+
+/**
+ * \brief Pobranie uprawnień pliku
+ * \param path ścieżka do pliku
+ * \return uprawnienia pliku
+ */
 int get_permission(const char* path);
 char* concat_path(const char* source, const char* file);
 int skip_location(const char* name);
